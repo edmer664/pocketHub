ec8dbc203ffbee77760a1560b8a57536e8de20a8
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-02-10 15:26:10 +0800

    fix: make user accessible to js

--- resources/views/messages/latestMessage.blade.php
+++ resources/views/messages/latestMessage.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+<script>
+    var user = {!! auth()->user()->toJson() !!};
+  </script>
 <div class="row vh-100 ">
     <div class="col-xs-2 col-sm-4 border-right border-primary">
         <div class="py-2">
@@ -16,54 +19,45 @@
                     </div>
                 </div>
             </form>
-
-            <div class="card border-0 py-4 ">
-                <div class=" border-bottom border-dark ">
-                    <div class="d-flex">
-                        <div class="pr-2">
-                            @if (Auth::user()->avatar_path == null)
-                            <img class="rounded-circle img-fluid m-3" width="60" height="60"
-                                src="https://avatars.dicebear.com/api/initials/{{substr(Auth::user()->first_name, 0, 1) . substr(Auth::user()->last_name, 0, 1)}}.svg?backgroundColorLevel=300&fontSize=35"
-                                alt="">
-                            @else
-                            <img class="rounded-circle img-fluid" width="60" height="60"
-                                src="{{ url('storage/avatars/' . Auth::user()->avatar_path) }}" alt="">
-                            @endif
-                        </div>
-                        <div class="pb-2">
-                            <span>Jane Doe</span>
-                            <p> Hi, I'm Fine Thank you <small class="pl-5">3 mins</small></p>
-                        </div>
-                    </div>
-                </div>
+            {{-- start --}}
+            <div id="conv-container">
+                {{-- container for list of contancts --}}
             </div>
+            
+            {{-- end --}}
         </div>
     </div>
 
     <div class="col  d-none d-sm-inline-block ">
         <div class="d-flex  flex-column  w-100 h-100 ">
             <div class="p-2 ">
-                <div py-5>
-                    <img src="img/undraw_profile.svg" class="rounded-circle float-left m-2 " width="50px"
+                <div class="py-5" id="conversation-details">
+                    <img id="user-image" src="img/undraw_profile.svg" class="rounded-circle float-left m-2 " width="50px"
                         alt="...">
-                    <h5 class="font-weight-bold p-3">Jane Doe</h5>
+                    <h5 id="user-name" class="font-weight-bold p-3"></h5>
                 </div>
                 <hr>
             </div>
-            <div class="p-2  h-100">
+            <div id="message-container" class="p-2  h-100 overflow-auto">
+
+                {{-- message style --}}
                 <div class="d-flex justify-content-start mb-4">
                     <div class="img_cont_msg">
                         <img src="img/undraw_profile.svg"
                         height="50px" width="50px" class="rounded-circle">
                     </div>
                     <p class="p-2  mx-2 my-1 rounded bg-secondary text-white">
-                        Hi, how are you jane?
+                        . . .
                         
                     </p>
                 </div>
+                {{-- end message style --}}
+
+
+                {{-- message stlye 2 --}}
                 <div class="d-flex justify-content-end mb-4">
                     <p class="p-2 my-1 mx-2 rounded bg-warning text-white">
-                        Hi, I'm Fine Thank you 
+                        . . .
                     </p>
                     <div class="img_cont_msg ">
                         <img src="img/undraw_profile.svg"
@@ -71,6 +65,7 @@
                         
                     </div>
                 </div>
+                {{-- end message stlye 2 --}}
             </div>
             
             <div class="align-items-end ">
@@ -89,4 +84,6 @@
         </div>
     </div>
 </div>
+
+<script src='{{ url('storage/message.js') }}'></script>
 @endsection
