f807ebca1259c0ed0fca108b35b1bdc319a83c49
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-02-10 15:25:01 +0800

    feat: add api endpoints

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,11 +5,17 @@ use App\Models\Message;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Conversation;
+use Illuminate\Support\Facades\Auth;
+
 
 class MessageController extends Controller
 {
     public function index(Request $request){
+        // return view
+        $user = Auth::user();
+        
         return view('messages.latestMessage');
+        
     }
     //send message
     public function send(Request $request){
@@ -20,31 +26,56 @@ class MessageController extends Controller
         $message->save();
         return redirect()->back();
     }
-
-    // show all messages
-    public function show(Request $request, $id){
-        // verify user if belongs to conversation
-        $conversation = Conversation::find($id);
-        if(!in_array($request->user()->id, $conversation->participants)){
-            return redirect()->route('home');
-        }
-        // retrieve messages from messages table
-        $messages = Message::where('conversation_id', $id)->orderBy('created_at', 'desc')->get();
-        foreach($messages as $message){
-            $message->user = User::find($message->sender_id);
-        }
-        return view('messages.latestMessage', compact('messages'));
-    }
     
-    // make a new conversation containing participants json
-    public function create(Request $request){
+    
+    // make a new conversation 
+    public function createCoversation(Request $request){
+        // check if conversation exist
+        $conversation = Conversation::where('participants', $request->user()->id)
+            ->where('participants', $request->receiver_id)->first();
+        if($conversation){
+            return redirect()->back();
+        }
+        // create new conversation
         $conversation = new Conversation();
-        // todo: parse request data to json
-        $conversation->participants = json_encode($request->participants);
+        $conversation->sender_id = $request->user()->id;
+        $conversation->receiver_id = $request->receiver_id;
         $conversation->save();
         return redirect()->back();
     }
 
-        
+    // api calls
+    // get conversations
+    public function getConversations(Request $request,$id){
+        // get conversations where user is sender or receiver
+        $conversations = Conversation::where('sender_id', $id)
+            ->orWhere('receiver_id', $id)->get();
+        // get last message for each conversation
+        foreach ($conversations as $conversation) {
+            $lastMessage = Message::where('conversation_id', $conversation->id)->orderBy('created_at', 'desc')->first();
+            
+            // if the sender_id is the current user id query the receiver_id
+            if($conversation->sender_id == $id){
+                $user = User::find($conversation->receiver_id);
+            }else{
+                $user = User::find($conversation->sender_id);
+            }
+            $conversation->lastMessage = $lastMessage;
+            $conversation->user = $user;
+        };
+
+        return response()->json($conversations);
+    }
+    // get messages in descending order
+    public function getMessages(Request $request, $id){
+        $messages = Message::where('conversation_id', $id)->orderBy('created_at', 'desc')->get();
+        return response()->json($messages);
+    }
+
+    
 
 }
+
+
+        
+
